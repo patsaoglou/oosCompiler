f3683fa25df2a007efea6e2bb95eaf461748d204
patsaoglou <panpat2002@gmail.com>
2024-10-25 02:46:55 +0300

    structs and basic constructor

--- oosListenerImplementation.py
+++ oosListenerImplementation.py
@@ -5,49 +5,130 @@ class oosListenerImplementation(oosListener):
 
     def __init__(self):
         self.output = []
+        self.known_classes = []
+        self.last_class_struct = None
 
         self.id_list = []
+        self.types_list = []
+        self.last_type= None
+
+        self.parlist = []
+        self.actual_pars = []
 
     def get_oos_compiled(self):
         return "".join(self.output)
 
     # --------------------------------------------
-   
-    def enterClass_def(self, ctx:oosParser.Class_defContext):
-        print("called: enterClass_def")
 
-        class_name = ctx.class_name(0).ID().getText()
-        self.output.append(f"typedef struct {class_name} {{\n")
+    def enterStartRule(self, ctx:oosParser.StartRuleContext):
+        # adding necessary C includes
+        self.output.append(f"#include <stdio.h>\n")
+        self.output.append(f"#include <stdlib.h>\n")
 
-    def exitClass_def(self, ctx:oosParser.Class_defContext):
-        class_name = ctx.class_name(0).ID().getText()
-        self.output.append(f"\n}} {class_name};\n")
 
     # --------------------------------------------
 
+    def enterClass_def(self, ctx:oosParser.Class_defContext):
+
+        class_name = ctx.class_name(0).ID().getText()
+        self.last_class_struct = class_name
+
+        self.output.append(f"\ntypedef struct {self.last_class_struct} {{")
+        self.known_classes.append(f"{self.last_class_struct}")
+        self.output.append("\n")
+      
+
+
+    def exitClass_def(self, ctx:oosParser.Class_defContext):
+        pass
+
+    # --------------------------------------------
+
+    def enterClass_body(self, ctx:oosParser.Class_bodyContext):
+        self.output.append(f"}} {self.last_class_struct};\n")
+    
+    # --------------------------------------------
+
+    def enterDeclarations(self, ctx:oosParser.DeclarationsContext):
+        pass
+
+    # Exit a parse tree produced by oosParser#declarations.
+    def exitDeclarations(self, ctx:oosParser.DeclarationsContext):
+        pass
+
+    
+    # --------------------------------------------
+
     def enterDecl_line(self, ctx:oosParser.Decl_lineContext):
-        print("called: enterDecl_line")
-        for id_object in ctx.ID():
-            self.id_list.append(f"{id_object.getText()}")
+        
+        if ctx.ID():
+            for id in ctx.ID():
+                self.id_list.append(f"{id.getText()}")
 
     def exitDecl_line(self, ctx:oosParser.Decl_lineContext):
-        pass
+        self.output.append("\t")
+
+        decl_type = self.types_list.pop()
+        self.output.append(f"{decl_type} ")
+        
+        if decl_type != "int":
+            # switct to pointers cause it is an object
+            self.id_list[:] = [f"*{id}" for id in self.id_list]
+            self.output.append(f"{", ".join(self.id_list)}")
+        else:
+            self.output.append(f"{", ".join(self.id_list)}")
+
+
+        self.output.append(";\n")
+
+        self.id_list = []
 
     # --------------------------------------------
    
     def enterTypes(self, ctx:oosParser.TypesContext):
-        print("called: enterTypes")
         if ctx.class_name():
-            pass
+            if (ctx.class_name().ID().getText() in self.known_classes):
+                self.types_list.append(f"{ctx.class_name().ID().getText()}")
+            else:
+                print(f"Cannot find class '{ctx.class_name().ID().getText()}'")
+                exit(0)
         elif ctx.getText() == "int":
-            self.output.append(f"\t{ctx.getText()} ")
-
-        if self.id_list:
-            self.output.append(", ".join(self.id_list))
-        self.id_list = []
+            self.types_list.append(f"int")
            
     def exitTypes(self, ctx:oosParser.TypesContext):
-        self.output.append(f";\n")
+        pass
 
     # --------------------------------------------
+
+    def enterConstructor_def(self, ctx:oosParser.Constructor_defContext):
+        constructor_class_name = ctx.class_name().ID().getText()
+        
+        if constructor_class_name in self.known_classes:
+            self.output.append(f"\n{constructor_class_name}* init${constructor_class_name}({constructor_class_name} *self$")
+            
+    def exitConstructor_def(self, ctx:oosParser.Constructor_defContext):
+        
+        # ti allo mporei
+        self.output.append(f"\n}}\n")
+
    
+    def enterParlist(self, ctx:oosParser.ParlistContext):
+        if ctx.ID():
+            for id in ctx.ID():
+                self.parlist.append(f"{id.getText()}")
+
+    # Exit a parse tree produced by oosParser#parlist.
+    def exitParlist(self, ctx:oosParser.ParlistContext):
+
+        for idx in range(len(self.types_list)):
+            
+            if self.types_list[idx] != "int":
+                self.output.append(f", {self.types_list[idx]} *{self.parlist[idx]}")
+            else:
+                self.output.append(f", {self.types_list[idx]} {self.parlist[idx]}")
+          
+        self.parlist = []
+        self.types_list = []
+
+        self.output.append(f")\n{{\n\t")
+         
